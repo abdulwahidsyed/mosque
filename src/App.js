30fed349127088445b5e2b3e6328e9840e565305
ABDUL WAHID SYED <abdulwahidsyed@Wahids-MacBook-Pro.local>
2022-03-12 21:46:42 +0530

    after deploying

--- src/App.js
+++ src/App.js
@@ -2,25 +2,13 @@ import logo from './logo.svg';
 import './App.css';
 import "react-datepicker/dist/react-datepicker.css";
 import Con from './Containers/Fillout/Fillout';
+import Header from './Containers/Header/Header';
 
 function App() {
   return (
     <div className="App">
+      <Header/>
       <Con />
-      {/* <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header> */}
     </div>
   );
 }
