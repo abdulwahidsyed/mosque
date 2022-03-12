114c10c986bf4d20cc3c55ec9b238f45b6ff8f06
ABDUL WAHID SYED <abdulwahidsyed@Wahids-MacBook-Pro.local>
2022-03-12 12:57:05 +0530

    first push

---
+++ src/Containers/Fillout/Modal/Modal.js
@@ -0,0 +1,37 @@
+import React from "react";
+import { Button, Modal } from "react-bootstrap";
+import "./Modal.css";
+
+const MyVerticallyCenteredModal = (props) => {
+  return (
+    <Modal
+      {...props}
+      size="lg"
+      aria-labelledby="contained-modal-title-vcenter"
+      centered
+    >
+      <Modal.Header closeButton>
+        <Modal.Title id="contained-modal-title-vcenter">
+          Please confirm given details
+        </Modal.Title>
+      </Modal.Header>
+      <Modal.Body>
+        <div className="data-table">
+          {props.formdata.map((el) => {
+            return (
+              <div className="row" key={el.placeholder}>
+                <p>{el.placeholder}</p>
+                <p>{el.value}</p>
+              </div>
+            );
+          })}
+        </div>
+      </Modal.Body>
+      <Modal.Footer>
+        <Button onClick={props.onSubmit}>Submit</Button>
+      </Modal.Footer>
+    </Modal>
+  );
+};
+
+export default MyVerticallyCenteredModal;
